698adc6770bde286b9dd3a7a49bd9580fff009cf
eythanSaillet <eythan.saillet@gmail.com>
2020-03-05 17:07:08 +0100

    :construction: setting html/css stroke effect on names

--- scripts/main.js
+++ scripts/main.js
@@ -1,76 +1,85 @@
 function lerp (start, end, ratio)
 {
-    return start * (1 - ratio) + ratio * end
+	return start * (1 - ratio) + ratio * end
 }
 
 let smoothScroll = 
 {
-    $body : document.querySelector('body'),
-    $content : document.querySelector('.menuContainer'),
-    $img : document.querySelector('.centerContainer img'),
-    $blackBlocs : document.querySelectorAll('.blackBloc'),
+	// DOM
+	$body : document.querySelector('body'),
+	$content : document.querySelector('.menuContainer'),
+	$img : document.querySelector('.centerContainer img'),
+	$blackBlocs : document.querySelectorAll('.blackBloc'),
+	$fillNamesContainer : document.querySelector('.fillNamesContainer'),
+	$strokeNamesContainer : document.querySelector('.strokeNamesContainer'),
 
-    contentHeight : null,
-    scrollValue : null,
-    scrollValueWithLerp : null,
+	contentHeight : null,
+	contentMarginTop : null,
+	contentMarginBottom : null,
+	scrollValue : null,
+	scrollValueWithLerp : null,
 
-    sizeBetweenImagesRatio : 0.15,
-    scrollSpeedRatio : 5,
+	// PARAMETERS
+	sizeBetweenImagesRatio : 0.20,
+	scrollSpeedRatio : 5,
 
-    setup()
-    {
-        this.setContentSize()
-        this.setScrollEvent()
-        this.setLerpInterval()
-        this.setResizeEvent()
-    },
+	setup()
+	{
+		this.setContentSize()
+		this.setScrollEvent()
+		this.setLerpInterval()
+		this.setResizeEvent()
+	},
 
-    setContentSize()
-    {
-        // SETTING MARGINS AND SIZES OF BLACKBLOCS TO PUT IMAGES AT THE CENTER OF THE VIEW
-        let marginTop = (window.innerHeight - this.$img.getBoundingClientRect().height) * 0.5
-        let marginBottom = (window.innerHeight - this.$img.getBoundingClientRect().height) * 0.5
-        this.$content.style.marginTop = `${marginTop}px`
-        this.$content.style.marginBottom = `${marginBottom}px`
-        
-        for (const _element of this.$blackBlocs)
-        {
-            _element.style.height = `${(window.innerHeight - this.$img.getBoundingClientRect().height) * this.sizeBetweenImagesRatio}px`
-        }
+	setContentSize()
+	{
+		// SETTING MARGINS AND SIZES OF BLACKBLOCS TO PUT IMAGES AT THE CENTER OF THE VIEW
+		this.contentMarginTop = (window.innerHeight - this.$img.getBoundingClientRect().height) * 0.5
+		this.contentMarginBottom = (window.innerHeight - this.$img.getBoundingClientRect().height) * 0.5
+		this.$content.style.marginTop = `${this.contentMarginTop}px`
+		this.$content.style.marginBottom = `${this.contentMarginBottom}px`
+		
+		for (const _element of this.$blackBlocs)
+		{
+			_element.style.height = `${(window.innerHeight - this.$img.getBoundingClientRect().height) * this.sizeBetweenImagesRatio}px`
+		}
 
 
-        // MAKE THE BODY AS LONG AS THE CONTENT TO PERMIT THE SCROLL
-        this.contentHeight = this.$content.getBoundingClientRect().height
-        // console.log(this.$content.getBoundingClientRect().height, marginTop, marginBottom, (this.$content.getBoundingClientRect().height + marginTop + marginBottom) * this.scrollSpeedRatio)
-        this.$body.style.height = `${(this.contentHeight + marginTop + marginBottom - window.innerHeight) * this.scrollSpeedRatio + window.innerHeight}px`
-    },
+		// MAKE THE BODY AS LONG AS THE CONTENT TO PERMIT THE SCROLL
+		this.contentHeight = this.$content.getBoundingClientRect().height
+		this.$body.style.height = `${(this.contentHeight + this.contentMarginTop + this.contentMarginBottom - window.innerHeight) * this.scrollSpeedRatio + window.innerHeight}px`
+	},
 
-    setScrollEvent()
-    {
-        // GET THE VALUE OF THE SCROLL
-        window.addEventListener('scroll', (_mouse) =>
-        {
-            this.scrollValue = window.pageYOffset
-            console.log(this.scrollValue)
-        })
-    },
+	setScrollEvent()
+	{
+		// GET THE VALUE OF THE SCROLL
+		window.addEventListener('scroll', (_mouse) =>
+		{
+			this.scrollValue = window.pageYOffset
+		})
+	},
 
-    setLerpInterval()
-    {
-        setInterval(() =>
-        {
-            // TRANSLATING BY THE SCROLL VALUE AFTER APPLY A LERP EFFECT
-            this.scrollValueWithLerp = lerp(this.scrollValueWithLerp, this.scrollValue, 0.2)
-            this.$content.style.transform = `translateY(${-this.scrollValueWithLerp / this.scrollSpeedRatio}px)`
-        }, 1000 / 60)
-    },
+	setLerpInterval()
+	{
+		setInterval(() =>
+		{
+			// TRANSLATING BY THE SCROLL VALUE AFTER APPLY A LERP EFFECT
+			this.scrollValueWithLerp = lerp(this.scrollValueWithLerp, this.scrollValue, 0.2)
+			this.$content.style.transform = `translateY(${-this.scrollValueWithLerp / this.scrollSpeedRatio}px)`
+			
+			// TRANSLATING NAMES TO SIMULATE A POSITION FIXED
+			this.$fillNamesContainer.style.transform = `translateY(${this.scrollValueWithLerp / this.scrollSpeedRatio - this.contentMarginTop}px)`
+			this.$strokeNamesContainer.style.transform = `translateY(${this.scrollValueWithLerp / this.scrollSpeedRatio - this.contentMarginTop}px)`
+		}, 1000 / 60)
+	},
 
-    setResizeEvent()
-    {
-        window.addEventListener('resize', () =>
-        {
-            this.setContentSize()
-        })
-    }
+	setResizeEvent()
+	{
+		// UPDATE CONTENT SIZE WHEN WINDOW RESIZE FOR RESPONSIVE
+		window.addEventListener('resize', () =>
+		{
+			this.setContentSize()
+		})
+	}
 }
 smoothScroll.setup()
